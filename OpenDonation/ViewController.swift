c84f52cdd2422c04884d8868833977a46bfa3637
sighmon <simon.loffler@gmail.com>
2018-10-20 17:55:03 +1100

    Tap label to make a donation.

--- OpenDonation/ViewController.swift
+++ OpenDonation/ViewController.swift
@@ -10,20 +10,35 @@ import UIKit
 import SquarePointOfSaleSDK
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var status: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Replace with your app's URL scheme.
-        let callbackURL = URL(string: "opendonation://callback")!
         
         // Your client ID is the same as your Square Application ID.
         // Note: You only need to set your client ID once, before creating your first request.
         SCCAPIRequest.setClientID(API.Square.API_CLIENT_ID)
         
+        // Make the label tappable to make a transaction
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.labelTapped))
+        status.addGestureRecognizer(tap)
+
+    }
+    
+    @objc func labelTapped(sender: UITapGestureRecognizer) {
+        // Make a transaction
+        makeTransaction(amount: 100)
+    }
+
+    func makeTransaction(amount: Int) {
         do {
+            // Replace with your app's URL scheme.
+            let callbackURL = URL(string: "opendonation://callback")!
+            
             // Specify the amount of money to charge.
-            let money = try SCCMoney(amountCents: 100, currencyCode: "AUD")
+            let money = try SCCMoney(amountCents: amount, currencyCode: "AUD")
             
             // Create the request.
             let apiRequest =
@@ -46,7 +61,5 @@ class ViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
-
-
 }
 
