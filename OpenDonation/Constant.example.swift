32d83893daed6f1abeacf16883a46c22e572f54f
sighmon <simon.loffler@gmail.com>
2018-10-21 17:10:44 +1100

    Rename this to Constant.swift and add your app ID.

---
+++ OpenDonation/Constant.example.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  OpenDonation
+//
+//  Created by Simon Loffler on 20/10/18.
+//  Copyright © 2018 Simon Loffler. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+    struct Square {
+        static let API_CLIENT_ID = "your-id"
+    }
+}
